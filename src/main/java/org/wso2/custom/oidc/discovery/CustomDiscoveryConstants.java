03bdf20a802c77c0c59d27abf86a650104230469
Sachith Manchanayaka <sachithm@wso2.com>
2023-02-02 21:16:26 -0300

    Create CustomDiscoveryConstants.java

---
+++ src/main/java/org/wso2/custom/oidc/discovery/CustomDiscoveryConstants.java
@@ -0,0 +1,17 @@
+package org.wso2.custom.oidc.discovery;
+
+
+/**
+ * X509 Certificate authenticator constants.
+ */
+public class CustomDiscoveryConstants {
+
+
+    /*
+     * Private Constructor will prevent the instantiation of this class directly.
+     */
+    private CustomDiscoveryConstants() {
+    }
+
+    public static final String SERVLET_URL = "/x509-certificate-servlet";
+}
